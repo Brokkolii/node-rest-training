18408a765d999bf85d0a414cc81b687b729b7f27
Patrick Girnghuber <p.girnghuber@yahoo.de>
2022-05-27 20:06:27 +0200

    changed pool call

--- db.js
+++ db.js
@@ -1,7 +1,13 @@
 const { Pool } = require('pg')
 require("dotenv").config();
 
-const pool = new Pool()
+const pool = new Pool({
+  user: process.env.PGUSERNAME,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+})
 //  uses .env variables
 //-----------------
 //  PGHOST=""
